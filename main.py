42ba61f5c1a47734b4bc9866f12065fbdd0de207
Vinicius Koga <viniciuskoga_m@hotmail.com>
2025-12-04 20:39:35 -0300

    sonegator 1.0

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ valor_frete_img = "imgs/valor_frete.png"
 salvar_2_img = "imgs/salvar_2.png"
 checkbox_img = "imgs/checkbox.png"
 enviar_nf_img = "imgs/enviar_nf.png"
-enviar_selec = "imgs/enviar_selec.png"
+enviar_selec_img = "imgs/enviar_selec.png"
 ok_img = "imgs/ok.png"
+nota_img = "imgs/nota.png"
 
 # Funçao que procura até encontrar todas as ocorrencias do parametro e retorna uma lista contendo todas.
 def find_occurrences(img):
-    time.sleep(1)
     while True:
         try:
             ocorrencias = list(autogui.locateAllOnScreen(img, confidence=0.9))
@@ -35,31 +35,32 @@ def find_occurrences(img):
                 return centros
         except ImageNotFoundException:
             print("procurando...")
-        time.sleep(1)
+        time.sleep(0.5)
 
 # Funçao que procura até encontrar a primeira ocorrencia do parametro e retorna ele.
 def find_occurrence(img):
-    time.sleep(1)
     while True:
         try:
             ocorrencia = autogui.locateCenterOnScreen(img, confidence=0.9)
             return ocorrencia
         except autogui.ImageNotFoundException:
             print("procurando...")
-        time.sleep(1)
+        time.sleep(0.5)
 
 # Lista todos os pendentes encontrados.
 while True:
     pendente_occurrences = find_occurrences(pendente_img)
     for pendente_pos in pendente_occurrences:
+        time.sleep(3)
+
         # Clica no pendente
-        find_occurrence(pendente_img)
         autogui.moveTo(pendente_pos)
         autogui.click()
 
         # Scroll down
-        find_occurrence(salvar_2_img)
+        find_occurrence(nota_img)
         autogui.scroll(-150)
+        time.sleep(1)
 
         # Lista todos os kits encontrados
         kit_occurrences = find_occurrences(kit_img)
@@ -68,6 +69,7 @@ while True:
             find_occurrence(kit_img)
             autogui.moveTo(kit_pos)
             autogui.click()
+            time.sleep(1)
 
             # Clica no NCM
             ncm_pos = find_occurrence(ncm_img)
@@ -75,7 +77,6 @@ while True:
             autogui.click()
 
             # Digita o NCM
-            time.sleep(0.2)
             autogui.hotkey('ctrl', 'a')
             autogui.press('backspace')
             autogui.write('3926.40.00')
@@ -84,7 +85,7 @@ while True:
             valor_pos = find_occurrence(valor_img)
             autogui.moveTo(valor_pos)
             autogui.click()
-            time.sleep(0.5)
+            time.sleep(1)
             autogui.click()
 
             # Copia o valor
@@ -120,12 +121,12 @@ while True:
             salvar_1_pos = find_occurrence(salvar_1_img)
             autogui.moveTo(salvar_1_pos)
             autogui.click()
+            time.sleep(3)
 
         # Clica em desconto
         desconto_pos = find_occurrence(desconto_img)
         autogui.moveTo(desconto_pos)
         autogui.click()
-        time.sleep(0.2)
 
         # Digita 0
         autogui.hotkey('ctrl', 'a')
@@ -136,7 +137,7 @@ while True:
         valor_frete_pos = find_occurrence(valor_frete_img)
         autogui.moveTo(valor_frete_pos)
         autogui.click()
-        time.sleep(2)
+        time.sleep(1)
         autogui.click()
 
         # Digita 0
@@ -145,31 +146,34 @@ while True:
         autogui.write('0', interval=0.05)
 
         # Scroll up
-        time.sleep(0.5)
         autogui.scroll(300)
+        time.sleep(1)
 
         # Clica em salvar_2
         salvar_2_pos = find_occurrence(salvar_2_img)
         autogui.moveTo(salvar_2_pos)
         autogui.click()
-        time.sleep(0.5)
+        time.sleep(1)
         autogui.click()
 
-    # Marcar checkbox e enviar
+    # Marca as checkboxes
     checkbox_ocurrences = find_occurrences(checkbox_img)
     for checkbox_pos in checkbox_ocurrences[1:]:
         autogui.moveTo(checkbox_pos)
         autogui.click()
         time.sleep(0.5)
 
+    # Clica em enviar notas
     enviar_nf_pos = find_occurrence(enviar_nf_img)
     autogui.moveTo(enviar_nf_pos)
     autogui.click()
 
-    enviar_selec_pos = find_occurrence(enviar_selec)
+    # Clica no botão Enviar
+    enviar_selec_pos = find_occurrence(enviar_selec_img)
     autogui.moveTo(enviar_selec_pos)
     autogui.click()
 
+    # Clica em Ok
     ok_pos = find_occurrence(ok_img)
     autogui.moveTo(ok_pos)
     autogui.click()
